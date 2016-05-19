fcde9080ae24bac5396ca7aecc888da6dc36c497
Yuma Murakami <ym-live@asagi.waseda.jp>
2016-05-19 16:31:47 +0900

    FSTDのテストの大きさをより大きく

--- tests/testthat/test-FSTDFixedNum.R
+++ tests/testthat/test-FSTDFixedNum.R
@@ -1,20 +1,20 @@
 context('FSTDFixedNum')
 
 test_that('3d FSTD',{
-		  tnsr <- array(1:216,dim = c(6,6,6))
+		  tnsr <- array(1:1000,dim = c(10,10,10))
 		  tnsr <- as.simple_sparse_array(tnsr)
-		  res <- FSTDFixedFNum(tnsr,2)
+		  res <- FSTDFixedFNum(tnsr,3)
 
-		  expect_equal(dim(res$g),c(4,4,4))
-		  expect_equal(dim(res$As[[1]]),c(6,4))
-		  expect_equal(dim(res$As[[2]]),c(6,4))
-		  expect_equal(dim(res$As[[3]]),c(6,4))
+		  expect_equal(dim(res$g),c(9,9,9))
+		  expect_equal(dim(res$As[[1]]),c(10,9))
+		  expect_equal(dim(res$As[[2]]),c(10,9))
+		  expect_equal(dim(res$As[[3]]),c(10,9))
 	
 		  expect_equal(class(res$g),'simple_sparse_array')
 		  expect_equal(class(res$As[[1]]),'simple_triplet_matrix')
 		  expect_equal(class(res$As[[2]]),'simple_triplet_matrix')
 		  expect_equal(class(res$As[[3]]),'simple_triplet_matrix')
 			
-
+		#TODO: seed固定で必ず同じ結果が返るように
 		 
 })
