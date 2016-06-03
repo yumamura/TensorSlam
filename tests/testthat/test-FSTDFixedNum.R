5f30d6419031b65401341ae1a91148cd8f266732
Yuma Murakami <ym-live@asagi.waseda.jp>
2016-06-03 15:52:12 +0900

    FSTD,FSTD-MUNTDの4次元テストスクリプトを追加

--- tests/testthat/test-FSTDFixedNum.R
+++ tests/testthat/test-FSTDFixedNum.R
@@ -18,3 +18,21 @@ test_that('3d FSTD',{
 		#TODO: FIX seed?
 		 
 })
+
+test_that('4d FSTD',{
+		  tnsr <- array(1:10000,dim = c(10,10,10,10))
+		  tnsr <- as.simple_sparse_array(tnsr)
+		  res <- FSTDFixedFNum(tnsr,3)
+
+		  expect_equal(dim(res$g),c(27,27,27,27))
+		  expect_equal(dim(res$As[[1]]),c(10,27))
+		  expect_equal(dim(res$As[[2]]),c(10,27))
+		  expect_equal(dim(res$As[[3]]),c(10,27))
+		  expect_equal(dim(res$As[[4]]),c(10,27))
+	
+		  expect_equal(class(res$g),'simple_sparse_array')
+		  expect_equal(class(res$As[[1]]),'simple_triplet_matrix')
+		  expect_equal(class(res$As[[2]]),'simple_triplet_matrix')
+		  expect_equal(class(res$As[[3]]),'simple_triplet_matrix')
+			
+})
--- tests/testthat/test-MUNTD.R
+++ tests/testthat/test-MUNTD.R
@@ -1,6 +1,6 @@
 context('MUNTD')
 
-test_that('HOSVD-MUNTD',{
+test_that('3d HOSVD-MUNTD',{
 		  tnsr <- as.simple_sparse_array(array(1:216,dim=c(6,6,6)))
 		  res <- MUNTD(tnsr,core_dims=c(2,2,2),lra_ranks=c(4,4,4),method='HOSVD')
 
@@ -15,7 +15,24 @@ test_that('HOSVD-MUNTD',{
 		  expect_equal(class(res$As[[3]]),'matrix') 
 })
 
-test_that('FSTD-MUNTD',{
+test_that('4d HOSVD-MUNTD',{
+		  tnsr <- as.simple_sparse_array(array(1:6^4,dim=c(6,6,6,6)))
+		  res <- MUNTD(tnsr,core_dims=c(2,2,2,2),lra_ranks=c(4,4,4,4),method='HOSVD')
+
+		  expect_equal(dim(res$g),c(2,2,2,2)) 
+		  expect_equal(dim(res$As[[1]]),c(6,2)) 
+		  expect_equal(dim(res$As[[2]]),c(6,2)) 
+		  expect_equal(dim(res$As[[3]]),c(6,2)) 
+		  expect_equal(dim(res$As[[4]]),c(6,2)) 
+
+		  expect_equal(class(res$g),'array') 
+		  expect_equal(class(res$As[[1]]),'matrix') 
+		  expect_equal(class(res$As[[2]]),'matrix') 
+		  expect_equal(class(res$As[[3]]),'matrix') 
+		  expect_equal(class(res$As[[4]]),'matrix') 
+})
+
+test_that('3d FSTD-MUNTD',{
 		  tnsr <- as.simple_sparse_array(array(1:1000,dim=c(10,10,10)))
 		  res <- MUNTD(tnsr,core_dims=c(2,2,2),lra_ranks=c(3),method='FSTD')
 
@@ -28,4 +45,21 @@ test_that('FSTD-MUNTD',{
 		  expect_equal(class(res$As[[1]]),'matrix') 
 		  expect_equal(class(res$As[[2]]),'matrix') 
 		  expect_equal(class(res$As[[3]]),'matrix') 
+})
+
+test_that('4d FSTD-MUNTD',{
+		  tnsr <- as.simple_sparse_array(array(1:10000,dim=c(10,10,10,10)))
+		  res <- MUNTD(tnsr,core_dims=c(2,2,2,2),lra_ranks=c(3),method='FSTD')
+
+		  expect_equal(dim(res$g),c(2,2,2,2)) 
+		  expect_equal(dim(res$As[[1]]),c(10,2)) 
+		  expect_equal(dim(res$As[[2]]),c(10,2)) 
+		  expect_equal(dim(res$As[[3]]),c(10,2)) 
+		  expect_equal(dim(res$As[[4]]),c(10,2)) 
+
+		  expect_equal(class(res$g),'array') 
+		  expect_equal(class(res$As[[1]]),'matrix') 
+		  expect_equal(class(res$As[[2]]),'matrix') 
+		  expect_equal(class(res$As[[3]]),'matrix') 
+		  expect_equal(class(res$As[[4]]),'matrix') 
 })
